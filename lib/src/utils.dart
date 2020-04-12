c4276dac6f6f865f193aa3aacd4dad28eaf31eed
Dana Hartweg <dana.hartweg@gmail.com>
2020-04-11 22:18:08 -0400

    Add util documentation

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,7 +1,10 @@
+/// Combines the give [property] and [value] into a storage key
+/// (`$property::$value`) to track active conditions.
 String generateConditionKey(String property, String value) {
   return '$property::$value';
 }
 
+/// Splits the storage key, returning the associated property and value strings.
 List<String> splitConditionKey(String conditionKey) {
   return conditionKey.split('::');
 }
