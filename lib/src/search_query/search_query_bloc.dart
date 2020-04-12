b39b96a5272828afe6d28e3827e25e8e7d0fa537
Dana Hartweg <dana.hartweg@gmail.com>
2020-04-11 22:34:42 -0400

    Add documentation for SearchQueryBloc

--- lib/src/search_query/search_query_bloc.dart
+++ lib/src/search_query/search_query_bloc.dart
@@ -5,6 +5,12 @@ import 'package:equatable/equatable.dart';
 
 part 'search_query_event.dart';
 
+/// Very simple bloc that simply acts as a holder for the current
+/// search query.
+///
+/// There should be no need to ever manually construct a [SearchQueryBloc].
+/// It should, instead, be retrieved from within the `BlocFilterSearchList`
+/// in order to dispatch events to add or clear the search query.
 class SearchQueryBloc extends Bloc<SearchQueryEvent, String> {
   @override
   String get initialState => '';
--- lib/src/search_query/search_query_event.dart
+++ lib/src/search_query/search_query_event.dart
@@ -1,19 +1,36 @@
 part of 'search_query_bloc.dart';
 
+/// {@template searchqueryevent}
+/// Base [FilterConditionsEvent] for extension.
+/// {@endtemplate}
 abstract class SearchQueryEvent extends Equatable {
+  /// {@macro searchqueryevent}
   const SearchQueryEvent();
 }
 
+/// {@template clearsearchquery}
+/// Dispatched from your UI to clear the current search query.
+///
+/// *Note:* this is functionally equivalent to setting the search query to
+/// an empty string.
+/// {@endtemplate}
 class ClearSearchQuery extends SearchQueryEvent {
+  /// {@macro clearsearchquery}
   const ClearSearchQuery();
 
   @override
   List<Object> get props => ['ClearSearchQuery'];
 }
 
+/// {@template setsearchquery}
+/// Dispatched from your UI to set a search query to be applied
+/// against the items from the source bloc.
+/// {@endtemplate}
 class SetSearchQuery extends SearchQueryEvent {
+  /// The search query to set to the state.
   final String query;
 
+  /// {@macro setsearchquery}
   const SetSearchQuery(this.query);
 
   @override
