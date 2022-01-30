a0807f5072ce2861a0a82e6b4cb09bb63ccdea84
Dana Hartweg <dana.hartweg@gmail.com>
2022-01-30 15:20:51 -0500

    Update example files (#28)
    * Update examples.
    
    * Remove effective dart from readme.
    
    * Missed a few readme updates.
    
    * Switch publisher.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,28 +16,28 @@ void main() {
     // Provide our source bloc to the remainder of the tree. In an actual app,
     // this would happen much close to where it was needed.
     BlocProvider<JournalEntryBloc>(
-      create: (_) => JournalEntryBloc()..add(_journalEntryEvent.load),
+      create: (_) => JournalEntryBloc()..add(LoadJournalEntry()),
       child: MaterialApp(
         title: 'Flutter Bloc List Manager',
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Flutter Bloc List Manager'),
+            title: const Text('Flutter Bloc List Manager'),
           ),
           body: ListManager<JournalEntry, Loaded, JournalEntryBloc>(
-            filterProperties: ['author', 'category', 'isPublished'],
-            searchProperties: ['content', 'description', 'title'],
+            filterProperties: const ['author', 'category', 'isPublished'],
+            searchProperties: const ['content', 'description', 'title'],
             child: Column(
               children: [
                 Row(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+                  children: const [
                     SearchInput(),
                     FilterConditionsLauncher(),
                   ],
                 ),
-                SizedBox(height: 10.0),
-                Expanded(
+                const SizedBox(height: 10.0),
+                const Expanded(
                   child: ItemListRenderer(),
                 ),
               ],
@@ -62,15 +62,15 @@ class FilterConditionGroup extends StatelessWidget {
   final Function(String property, String value) isOptionActive;
   final Function updateCondition;
 
-  FilterConditionGroup({
+  const FilterConditionGroup({
     required this.condition,
     required this.isOptionActive,
     required this.updateCondition,
-    Key key,
+    Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(_) {
+  Widget build(context) {
     return Container(
       key: ValueKey(condition.key),
       child: Column(
@@ -78,8 +78,8 @@ class FilterConditionGroup extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            _filterPropertyLabelMap[condition.key],
-            style: TextStyle(
+            _filterPropertyLabelMap[condition.key]!,
+            style: const TextStyle(
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -102,10 +102,12 @@ class FilterConditionGroup extends StatelessWidget {
 // State of the source bloc that indicates items have been loaded
 // and are ready for further processing.
 class FilterConditionsLauncher extends StatelessWidget {
+  const FilterConditionsLauncher({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: Icon(Icons.filter_list),
+      icon: const Icon(Icons.filter_list),
       onPressed: () {
         showModalBottomSheet(
           context: context,
@@ -126,16 +128,16 @@ class FilterConditionsSheet extends StatelessWidget {
   // context will now belong to the Scaffold rendering the bottom sheet.
   final FilterConditionsBloc _filterConditionsBloc;
 
-  const FilterConditionsSheet({required filterConditionsBloc})
-      : assert(filterConditionsBloc != null),
-        _filterConditionsBloc = filterConditionsBloc;
+  const FilterConditionsSheet({required filterConditionsBloc, Key? key})
+      : _filterConditionsBloc = filterConditionsBloc,
+        super(key: key);
 
   // Helper to avoid duplication in the child components and to avoid
   // having to pass the bloc down another level.
   // Handles toggling property/value pair in the filter conditions bloc.
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: MediaQuery.of(context).size.height * 0.3,
       child: BlocBuilder<FilterConditionsBloc, FilterConditionsState>(
         bloc: _filterConditionsBloc,
@@ -159,7 +161,7 @@ class FilterConditionsSheet extends StatelessWidget {
             );
           }
 
-          return CircularProgressIndicator();
+          return const CircularProgressIndicator();
         },
       ),
     );
@@ -183,16 +185,18 @@ class FilterConditionsSheet extends StatelessWidget {
 }
 
 class ItemListRenderer extends StatelessWidget {
+  const ItemListRenderer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ItemListBloc, ItemListState>(
       builder: (_, state) {
         if (state is NoSourceItems) {
-          return Text('No source items');
+          return const Text('No source items');
         }
 
         if (state is ItemEmptyState) {
-          return Text('No matching results');
+          return const Text('No matching results');
         }
 
         if (state is ItemResults<JournalEntry>) {
@@ -225,13 +229,13 @@ class JournalEntry extends Equatable implements ItemClassWithAccessor {
   final bool isPublished;
 
   const JournalEntry({
-    this.author,
-    this.category,
-    this.content,
-    this.description,
-    this.id,
-    this.title,
-    this.isPublished,
+    required this.author,
+    required this.category,
+    required this.content,
+    required this.description,
+    required this.id,
+    required this.title,
+    required this.isPublished,
   });
 
   // Every prop intended to be used in a filtering or sorting operation
@@ -240,26 +244,21 @@ class JournalEntry extends Equatable implements ItemClassWithAccessor {
   List<Object> get props =>
       [author, content, description, id, title, isPublished];
 
+  @override
   dynamic operator [](String prop) {
     switch (prop) {
       case 'author':
         return author;
-        break;
       case 'category':
         return category;
-        break;
       case 'content':
         return content;
-        break;
       case 'description':
         return description;
-        break;
       case 'title':
         return title;
-        break;
       case 'isPublished':
         return isPublished;
-        break;
       default:
         throw ArgumentError('Property `$prop` does not exist on JournalEntry.');
     }
@@ -267,58 +266,55 @@ class JournalEntry extends Equatable implements ItemClassWithAccessor {
 }
 
 // Render an input that will funnel the value into the SearchQueryCubit.
-class JournalEntryBloc extends Bloc<_journalEntryEvent, JournalEntryState> {
-  JournalEntryBloc() : super(Loading());
-
-  @override
-  Stream<JournalEntryState> mapEventToState(
-    _journalEntryEvent event,
-  ) async* {
-    if (event == _journalEntryEvent.load) {
-      // Stub data for the example, you would likely be using some sort
-      // of repository here to fetch your data.
-      yield Loaded([
-        JournalEntry(
-          author: 'Author 1',
-          category: 'Category 1',
-          content: 'Content 1',
-          description: 'Description 1',
-          id: '1',
-          title: 'Title 1',
-          isPublished: true,
-        ),
-        JournalEntry(
-          author: 'Author 2',
-          category: 'Category 2',
-          content: 'Content 2',
-          description: 'Description 2',
-          id: '2',
-          title: 'Title 2',
-          isPublished: false,
-        ),
-        JournalEntry(
-          author: 'Author 3',
-          category: 'Category 3',
-          content: 'Content 3',
-          description: 'Description 3',
-          id: '3',
-          title: 'Title 3',
-          isPublished: true,
-        )
-      ]);
-    }
+class JournalEntryBloc extends Bloc<JournalEntryEvent, JournalEntryState> {
+  JournalEntryBloc() : super(Loading()) {
+    on<LoadJournalEntry>((event, emit) => emit(const Loaded([
+          JournalEntry(
+            author: 'Author 1',
+            category: 'Category 1',
+            content: 'Content 1',
+            description: 'Description 1',
+            id: '1',
+            title: 'Title 1',
+            isPublished: true,
+          ),
+          JournalEntry(
+            author: 'Author 2',
+            category: 'Category 2',
+            content: 'Content 2',
+            description: 'Description 2',
+            id: '2',
+            title: 'Title 2',
+            isPublished: false,
+          ),
+          JournalEntry(
+            author: 'Author 3',
+            category: 'Category 3',
+            content: 'Content 3',
+            description: 'Description 3',
+            id: '3',
+            title: 'Title 3',
+            isPublished: true,
+          )
+        ])));
   }
 }
 
 // Render an icon button that will launch the filter conditions UI sheet
 // into the current scaffold.
+abstract class JournalEntryEvent {}
+
+// Hooks into the `FilterConditionsBloc` in order to render the filtering UI.
 abstract class JournalEntryState extends Equatable {
   const JournalEntryState();
 }
 
-// Hooks into the `FilterConditionsBloc` in order to render the filtering UI.
+// As we've built a UI around filtering, we need display-friendly
+// labels for the underlying property names.
+// This could easily be provided statically by the base item class instead.
 class Loaded extends JournalEntryState
     implements ItemSourceState<JournalEntry> {
+  @override
   final List<JournalEntry> items;
 
   const Loaded(this.items);
@@ -327,19 +323,22 @@ class Loaded extends JournalEntryState
   List<Object> get props => [items];
 }
 
-// As we've built a UI around filtering, we need display-friendly
-// labels for the underlying property names.
-// This could easily be provided statically by the base item class instead.
+// Essentially just a pass-through widget to simplify the rendering
+// of each condition group.
 class Loading extends JournalEntryState {
   @override
   List<Object> get props => ['Loading'];
 }
 
-// Essentially just a pass-through widget to simplify the rendering
-// of each condition group.
+// Hooks into the state from the `ItemListBloc` and renders the list
+// portion of the UI.
+class LoadJournalEntry extends JournalEntryEvent {}
+
 class SearchInput extends StatelessWidget {
+  const SearchInput({Key? key}) : super(key: key);
+
   @override
-  Widget build(_) {
+  Widget build(context) {
     return BlocBuilder<SearchQueryCubit, String>(
       builder: (context, state) {
         return Flexible(
@@ -357,7 +356,3 @@ class SearchInput extends StatelessWidget {
     );
   }
 }
-
-// Hooks into the state from the `ItemListBloc` and renders the list
-// portion of the UI.
-enum _journalEntryEvent { load }
